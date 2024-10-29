b48f3fce1537ca8be95717a5ca309b222c336b69
mohamed <mohamedkamouss@proton.me>
2024-10-29 20:56:47 +0000

    TP1 PART 3

--- TP1/src/main/java/org/example/metier/EtudiantManager.java
+++ TP1/src/main/java/org/example/metier/EtudiantManager.java
@@ -2,23 +2,42 @@ package org.example.metier;
 
 import org.example.dao.Etudiant;
 import org.example.dao.EtudiantDAO;
+import org.example.dao.IEtudiantDAO;
 
 
 import java.util.List;
 
 public class EtudiantManager {
-    public EtudiantDAO etudiantDAO;
-    public Etudiant addEtudiant(Etudiant etudiant) {
-        List<String>etudiantEmails = etudiantDAO.getAllEmails();
-        if (etudiantEmails.contains(etudiant.getEmail())) {
-            return null;
-        }
-        else{
-            etudiantDAO.addEtudiant(etudiant);
-            return etudiant;
-        }
+    public IEtudiantDAO iEtudiantDAO;
+
+    public EtudiantManager(IEtudiantDAO iEtudiantDAO) {
+        this.iEtudiantDAO = iEtudiantDAO;
     }
+
+    public Etudiant addEtudiant(Etudiant etudiant){
+        int count = 0;
+        if(!iEtudiantDAO.getAllEtudiant().isEmpty()){
+            for (Etudiant etud : iEtudiantDAO.getAllEtudiant()){
+                if(etud.getEmail() == etudiant.getEmail()){
+                    count++;
+                }
+            }
+            if(count > 0){
+                System.out.println("L'email que vous avez saisies est déjà utilisé par un étudiant ");
+            }else{
+                iEtudiantDAO.addEtudiant(etudiant);
+            }
+        }else{
+            iEtudiantDAO.addEtudiant(etudiant);
+        }
+        return etudiant;
+    }
+
+    public Etudiant updateEtudiant(Etudiant etudiant){
+        return iEtudiantDAO.updateEtudiant(etudiant);
+    }
+
     public List<Etudiant> getAllEtudiants(){
-        return etudiantDAO.getAllEtudiants();
+        return iEtudiantDAO.getAllEtudiant();
     }
 }
