700e67aebb92580f721d7682509da740ee03d4fd
mohamed <mohamedkamouss@proton.me>
2024-11-10 17:49:37 +0000

    TP GRAPHQL PART2

---
+++ TPGRAPHQL/video-service/inventory-service/src/main/java/org/example/inventoryservice/web/VideoGraphQlController.java
@@ -0,0 +1,30 @@
+package org.example.inventoryservice.web;
+
+import org.example.inventoryservice.dao.entites.Creator;
+import org.example.inventoryservice.dao.entites.Video;
+import org.example.inventoryservice.dao.repositories.CreatorRepository;
+import org.example.inventoryservice.dao.repositories.VideoRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class VideoGraphQlController {
+    private CreatorRepository creatorRepository;
+    private VideoRepository videoRepository;
+    VideoGraphQlController(CreatorRepository creatorRepository, VideoRepository videoRepository){
+        this.creatorRepository = creatorRepository;
+        this.videoRepository = videoRepository;
+    }
+    @QueryMapping
+    public List<Video> videoList(){
+        return videoRepository.findAll();
+    }
+    @QueryMapping
+    public Creator creatorById(@Argument Long id) {
+        return creatorRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("Creator %s not found",id)));
+    }
+}
